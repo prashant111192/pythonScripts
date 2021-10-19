e3bb8f61d21189237865c616aa27e8f7f202010e
Prashant Kumar <prashant_1111@hotmail.com>
2021-10-19 16:45:13 +0200

    l2 error

--- L2Error/l2-2.py
+++ L2Error/l2-2.py
@@ -6,6 +6,7 @@ import csv
 import os
 import statistics as stat
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 
 # Input sphase and fluent files
@@ -16,7 +17,8 @@ fluent = np.loadtxt('./FluentDigester.csv', delimiter = ',', skiprows =1)
 # fluent = np.array(b)
 
 
-name = ["DSPH.015.015.100.csv", "DSPH.015.0225.100.csv","dsph.csv","DSPH.015.045.100.csv", "DSPH.015.060.100.csv"]
+# name = ["DSPH.015.015.100.csv", "DSPH.015.0225.100.csv","dsph.csv","DSPH.015.045.100.csv", "DSPH.015.060.100.csv"]
+name = ["DSPH.015.015.100.csv", "DSPH.015.0225.100.csv","dsph.csv","DSPH.015.045.100.csv", "DSPH.015.060.100.csv", "DSPH.015.120.100.csv"]
 # name = "dsph.csv"
 # name = "DSPH.015.060.100.csv"
 l2_sphase = np.ones(len(name))
@@ -34,12 +36,16 @@ for i in range (len(name)):
     dsph_inter_sphase = np.interp(sphase[:,0], dsph[:,0], dsph[:,1])
     dsph_inter_fluent = np.interp(fluent[:,0], dsph[:,0], dsph[:,1])
 
-    l2_sphase[i] = np.power((np.sum(np.power((sphase[:,1]-dsph_inter_sphase),2)))/len(sphase[:,1]),0.5)
-    l2_fluent[i] = np.power((np.sum(np.power((fluent[:,1]-dsph_inter_fluent),2)))/len(dsph_inter_fluent),.5)
+    l2_sphase[i] = np.power((np.sum(np.power((sphase[:,1]-dsph_inter_sphase),2))),.5)
+    l2_fluent[i] = np.power((np.sum(np.power((fluent[:,1]-dsph_inter_fluent),2))),.5)
+    # l2_sphase[i] = np.power((np.sum(np.power((sphase[:,1]-dsph_inter_sphase),2)))/len(sphase[:,1]),0.5)
+    # l2_fluent[i] = np.power((np.sum(np.power((fluent[:,1]-dsph_inter_fluent),2)))/len(dsph_inter_fluent),.5)
     # l2_sphase[i] = np.power(np.sum(np.power((sphase[:,1]-dsph_inter_sphase),2))/len(sphase[:,1]),0.5)
     # l2_fluent[i] = np.power(np.sum(np.power((fluent[:,1]-dsph_inter_fluent),2))/len(fluent[:,1]),0.5)
-    l1_sphase[i] = np.sum(np.absolute((sphase[:,1]-dsph_inter_sphase)))/len(sphase[:,1])
-    l1_fluent[i] = np.sum(np.absolute((fluent[:,1]-dsph_inter_fluent)))/len(fluent[:,1])
+    # l1_sphase[i] = np.sum(np.absolute((sphase[:,1]-dsph_inter_sphase)))/len(sphase[:,1])
+    # l1_fluent[i] = np.sum(np.absolute((fluent[:,1]-dsph_inter_fluent)))/len(fluent[:,1])
+    l1_sphase[i] = np.sum(np.absolute((sphase[:,1]-dsph_inter_sphase)))
+    l1_fluent[i] = np.sum(np.absolute((fluent[:,1]-dsph_inter_fluent)))
     temp1 = np.power(dsph[:,1],2)
     temp2 = np.power(sphase[:,1],2)
     ar_sphase[i] = np.power(((np.sum(temp1))/(np.sum(temp2))),.5)
@@ -53,33 +59,54 @@ for i in range (len(name)):
 
 # time = [1206, 205, 34]
 # time = np.array([1134549, 286430, 73001, 18928])
-time = np.array([0.015, 0.03, 0.06, 0.12])
-plt.scatter(time, l2_sphase, label="sphase")
-plt.scatter(time, l2_fluent, label = "fluent")
+# time = np.array([0.015, 0.0225, 0.03, 0.045,0.06]).reshape(-1,1)
+time = np.array([0.015, 0.0225, 0.03, 0.045,0.06, 0.12]).reshape(-1,1)
+time = time*1
+
+errorSphase = l2_sphase
+errorFluent = l2_fluent
+model = LinearRegression()
+model.fit(time, errorFluent)
+y_pred = model.predict(time)
+# sp = csaps.UnivariateCubicSmoothingSpline(x, y, smooth=0.85)
+
+
+print(model.coef_)
+plt.plot(time, (model.coef_*time+model.intercept_))
+plt.scatter(time, errorSphase, label="sphase")
+plt.scatter(time, errorFluent, label = "fluent")
 axes = plt.gca()
 # y_vals = np.array(l2_fluent)
+plt.xlim(0.01,1)
 x_vals = np.array(axes.get_xlim())
-intercept = 0.5*l2_sphase[0]
+intercept = model.intercept_
+# intercept = 0.5*l2_sphase[0]
 # intercept = 0
 # intercept = -1*np.power(l2_sphase[0],1000000000)
-y_vals = intercept + 2 * x_vals
+y_vals = (1) + 2 * x_vals
+# y_vals = (intercept+.1) + 2 * x_vals
 # x_vals = intercept + y_vals/2
-plt.plot(x_vals, y_vals, '--', label = "2")
+plt.plot(x_vals, y_vals, '-.', label = "2nd order convergence")
 # x_vals = intercept + y_vals/1
-y_vals = intercept + 1 * x_vals
-plt.plot(x_vals, y_vals, '--', label="1")
+y_vals = (0.1) + 1 * x_vals
+plt.plot(x_vals, y_vals, '--', label="1st order convergence")
 # y_vals = intercept + 3 * x_vals
 # plt.plot(x_vals, y_vals, '--', label="3")
-plt.ylim(0.5*np.min(l2_sphase),2*np.max(l2_sphase))
+plt.ylim(0.1,10)
+plt.xlim(0.01,1)
+# plt.ylim(0.5*np.min(errorSphase),2*np.max(errorSphase))
 plt.yscale('log')
+plt.xscale('log')
+plt.grid(which="minor")
+plt.grid(which="major")
 plt.legend()
 plt.savefig("xx.png")
 plt.clf()
 # time = np.array([1134549, 286430, 73001, 18928])
-plt.plot(time, l1_sphase, label="sphase")
-plt.plot(time, l1_fluent, label = "fluent")
+plt.plot(time, l1_sphase, label="V/s SPHASE")
+plt.plot(time, l1_fluent, label = "V/s Fluent")
 plt.legend()
-# plt.yscale('log')
+plt.yscale('log')
 plt.savefig("mesh.png")
 # plt.show()
 
@@ -92,6 +119,8 @@ temp2 = np.power(fluent[:,1],2)
 ar_sf = np.power(((np.sum(temp1))/(np.sum(temp2))),.5)
 temp1 = (np.sum(np.power(sph_fluent-fluent[:,1],2)))/(np.sum(np.power(sph_fluent,2)))
 pd_sf = np.power(temp1,.5)
+
+
 
 print(l1_sphase_fluent, l2_sphase_fluent)
 print("fluent")
@@ -128,3 +157,5 @@ print(ar_sf)
 # plt.plot(xloc,vavg, 'g:', label='DSPH')
 # np.savetxt("./dsph.csv", np.stack((xloc, vavg), axis=1), fmt="%f", delimiter=",", comments="", header="xloc,vavg")
 
+
+# https://www.sciencedirect.com/science/article/pii/S0927025616304426
